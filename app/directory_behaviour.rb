8719d1c7dad844d494b4f10a9a9421cf1cbaf5a5
Andrea Silva <andiesilvamachado@gmail.com>
2020-07-23 19:08:29 -0500

    Add fixes from rubocop

--- app/directory_behaviour.rb
+++ app/directory_behaviour.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
+
 require_relative 'flag_behaviour'
 
 # Parent class of any flag
 class DirectoryBehaviour < FlagBehaviour
   def initialize(flag_exists, argv)
+    super
     @name = 'Directory'
-    @value = flag_exists
-    @arguments = argv
   end
-  
+
   def identify_value
     position = @arguments.index('-d')
-    if /^(\/[\w^ ]+)+\/?$/.match(@arguments[position + 1]) != nil
+    if /^(\/[\w^ ]+)+\/?$/.match(@arguments[position + 1]) != nil && flag?(@arguments[position + 1]) == false
       @value = @arguments[position + 1]
+    elsif @flags.include?(@arguments[position + 1]) || @arguments[position + 1] == nil
+      @value = ''
     else
-      @value = "Error, given value is not a valid path"
+      @value = 'Error, given value is not a valid path'
     end
   end
 end
--- app/flag_behaviour.rb
+++ app/flag_behaviour.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
+require_relative 'schema'
+
 # Parent class of any flag
 class FlagBehaviour
-  def identify_value
+  def initialize(flag_exists, argv)
+    @value = flag_exists
+    @arguments = argv
+    @flags = ['-l', '-p', '-d']
   end
+
+  def identify_value; end
 
   def create_flag_schema
     identify_value
     Schema.new(@name, @value)
+  end
+
+  def flag?(argument)
+    @flags.include?(argument)
   end
 end
